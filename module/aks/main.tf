6de6fc85f8ee715eddf60cbfbb457cdc8f10d278
pimncn01 <netchanok.m@cloudnatician.com>
2023-10-04 11:06:18 +0700

    add dockerfile

--- module/aks/main.tf
+++ module/aks/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     vm_size    = "Standard_D2_v2"
-    node_count = 1
+    node_count = 3
 
   }
   network_profile {
